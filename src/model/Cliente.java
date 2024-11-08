17271d25eb3e4a6fdc576c49d5dd0472f8f4dbe0
Cauê Cunha <108994245+ccaue@users.noreply.github.com>
2024-11-07 23:35:27 -0300

    Add files via upload

---
+++ src/model/Cliente.java
@@ -0,0 +1,15 @@
+package com.biblioteca.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+}
