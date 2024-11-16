d5f93a7918d178f2dee74c76f78bc0effd049701
Cauê Cunha <108994245+ccaue@users.noreply.github.com>
2024-11-16 20:34:52 -0300

    Create Cliente.java

---
+++ src/com.biblioteca.model/Cliente.java
@@ -0,0 +1,25 @@
+package com.biblioteca.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="nome", length = 100, unique = true)
+    private String nome;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="telefone")
+    private String telefone;
+    
+    @Column(name="endereco")
+    private String endereco;
+}
